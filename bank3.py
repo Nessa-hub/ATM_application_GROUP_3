f7716199ab533c0e81e421f7b915bf3c1d0f53d3
Nessa-hub <52928882+Nessa-hub@users.noreply.github.com>
2019-07-31 12:37:42 +0000

    edited login confirmation, account, savings and checking classes
    In the login confirmation, i included an option for the user to choose between checking account or savings account. I also included the account,savings and checking classes that I created. It might need more editing from you though. Thanks.

--- bank3.py
+++ bank3.py
@@ -56,20 +56,28 @@ class ATM:
         if ATM.logIn_confirmation == True:
             while 1:
                 print("WELCOME TO PSW2019 GROUP02 BANK ATM.")
-                print("ENTER 1 TO CHECK ACCOUNT BALANCE, 2 TO DEPOSIT AND 3 TO WITHDRAW, 4 TO LOG OUT")
-                menuChoice = int(input("enter your option: " ))
+                accountType = int(input("1: Savings\n 2: Checking\n "))
+                
+                if accountType == 1 or accountType == 2
+                    print("1: CHECK BALANCE \n 2: DEPOSIT\n 3: WITHDRAW\n  4: LOG OUT\n")
+                    menuChoice = int(input("enter your option: " ))
             
-                if (menuChoice == 1):
-                    testAccount = Account("savings", "group02")
-                    testAccount.checkBalance()
+                    if (menuChoice == 1):
+                        testAccount = Account("savings", "group02")
+                        testAccount.checkBalance()
                     
                     
-                elif (menuChoice == 2):
-                    testAccount = withdrawal_transaction()
-                    testAccount.withdrawal()
+                    elif (menuChoice == 2):
+                        testAccount = deposit_transaction()
+                        testAccount.deposit()
                     
-                elif (menuChoice == 3):
-                    ATM.logIn()
+                    elif (menuChoice == 3):
+                        testAccount = withdrawal_transaction()
+                        testAccount.withdrawal()
+                    
+                    elif (menuChoice == 4):
+                        ATM.logOut()
+                    
         if ATM.logIn_confirmation == False:
             exit()
 
@@ -80,6 +88,9 @@ class atmTransaction:
     type = None
     def update(self):
         print("this is the atm transaction class")
+        #balance = balance - amount
+        #return balance
+        
 
 class withdrawal_transaction(atmTransaction):
     # pin = 1234
@@ -124,34 +135,57 @@ class Customer:
         print("this is the customer class")
 
 class Account:
-    balance  = 3000
+    def __init__(self,type,owner):
+        self.type = type
+        self.owner = owner
 
-    def __init__(self, types,owner):
-        self.types = "savings"
-        self.owner = "group02"
+    def checkBalance(self,accountNo):
+        balance = 6000
+        #link balance after withdrawal and deposit to balance somewhere so that
+        # it can simply be inherited and kept up to date all the time
+        print("Your balance is: ".format(balance))
 
-    def checkBalance(self):
-        print("BALANCE: ", Account.balance)
+
+    #account_type = input("1: Savings \n 2: Checking: ")
+
+class Checking(Account):
+    def __init__(self,accountNo):
+        self.accountNo = accountNo
+
+    def debit(self,accountNo):
+        #is pin check necessary?
+        #how do i make sure it's debiting from the right account?
+        #Assumption: Debit account due to payment for bills,shopping,monthly deductions etc
+        debitAmount = int(input("Enter amount: "))
+        Account.balance = Account.balance - debitAmount
+        print("{} has been deducted successfully from your account".format(debitAmount))
+
+    def credit(self,accountNo,creditAmount):
+        self.creditAmount = creditAmount
+
+        print("Your account has been credited with {}".format(creditAmount))
 
 
-#in direct association debit card class and customer class and the ATM transaction class.
-class savingAccounts(Account):
-    accountNo = Bank.accountNo
+class Savings(Account):
+    def __init__(self,accountNo):
+        self.accountNo = accountNo
 
-    def debit(self):
-        print("print this is the savings account class")
+    def debit(self,accountNo,debitNo):
+        maxDebit = 6 #The maximum number of times an individual is allowed
+        #to have their account debited
+        #debitNo = number of debits done within the month
+        if debitNo < 6:
+            debitAmount = int(input("Enter amount: "))
+            Account.balance = Account.balance - debitAmount
+            print("{} has been deducted successfully from your account".format(debitAmount))
+        else:
+            print("You have exceeded your limit for the month.\n Please try again next month")
 
-    def credit(self):
-        print("this is the checkingAccount class")
 
-class checkingAccount(Account):
-    accountNo = Bank.accountNo
+    def credit(self,accountNo,creditAmount):
+        self.creditAmount = creditAmount
 
-    def debit(self):
-        print("this is the checkingAccount class")
-
-    def credit(self):
-        print("this is the checkingAccount class")
+        print("Your account has been credited with {}".format(creditAmount))
 
 group02ATM = ATM("GRP0002", 1234)
 group02ATM.logIn()
